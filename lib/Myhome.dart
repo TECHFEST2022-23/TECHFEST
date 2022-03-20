8b7e74b3fa3c3615427690735a999b90e0616843
SavanGangani <savangangani8518@gmail.com>
2022-03-20 10:55:35 +0530

    changes in bottombar

--- lib/Myhome.dart
+++ lib/Myhome.dart
@@ -24,6 +24,14 @@ class _MyHomeState extends State<MyHome> {
     About(),
     ProfilePage()
   ];
+
+  List<IconData> listOfIcons = [
+    FontAwesomeIcons.home,
+    Icons.calendar_today,
+    FontAwesomeIcons.info,
+    FontAwesomeIcons.user,
+  ];
+
   var currentIndex = 0 ;
   void onTab(int index){
     setState(() {
@@ -32,8 +40,7 @@ class _MyHomeState extends State<MyHome> {
   }
   @override
   Widget build(BuildContext context) {
-
-
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -46,22 +53,70 @@ class _MyHomeState extends State<MyHome> {
       ),
       drawer: Mydrawer(),
       body: page[currentIndex],
-      bottomNavigationBar: SlidingClippedNavBar(
-        iconSize: 19,
-        onButtonPressed: onTab,
-        activeColor: Colors.lightBlue,
-        selectedIndex: currentIndex,
-        barItems: [
-          BarItem(title: "Home", icon: Icons.home),
-          BarItem(title: "Event", icon: Icons.calendar_today),
-          BarItem(title: "About", icon: FontAwesomeIcons.info),
-          BarItem(title: "Profile", icon: FontAwesomeIcons.user),
-        ],
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.all(09),
+        height: size.width * .135,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.15),
+              blurRadius: 30,
+              offset: Offset(5, 5),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: ListView.builder(
+          itemCount: 4,
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(horizontal: size.width * .024),
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {
+              setState(
+                    () {
+                  currentIndex = index;
+                },
+              );
+            },
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 0),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  margin: EdgeInsets.only(
+                    bottom: index == currentIndex ? 0 : size.width * .029,
+                    right: size.width * .0422,
+                    left: size.width * .0422,
+                  ),
+                  width: size.width * .128,
+                  height: index == currentIndex ? size.width * .014 : 0,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(10),
+                    ),
+                  ),
+                ),
+                Icon(
+                  listOfIcons[index],
+                  size: size.width * .06,
+                  color: index == currentIndex
+                      ? Colors.blueAccent
+                      : Colors.black38,
+                ),
+                SizedBox(height: size.width * .03),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
 
 
 
